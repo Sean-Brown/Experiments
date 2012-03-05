a7a761ec0857922e98dd86f3a2850b9b5620acd8
Peter Himschoot <peter.himschoot@gmail.com>
2012-03-05 11:35:18 +0100

    Test comitting code from XCode

---
+++ HelloObjectiveC/HelloObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by Peter Himschoot on 05/03/12.
+//  Copyright (c) 2012 U2U. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+  
+  @autoreleasepool {
+    
+    // insert code here...
+    NSLog(@"Hello, World!");
+    
+  }
+  return 0;
+}
+
